9f9ba3ee64fd959261bf4d8dec3fa6302f85e370
piwiit <bouillaux@hotmail.fr>
2023-04-16 11:49:34 +0200

    remake all project

--- src/js/module/loading-page.js
+++ src/js/module/loading-page.js
@@ -1,51 +1,30 @@
 const createLogo = () => {
-  const logo = document.createElement("img");
-  logo.src =
-    "https://laboussole-larochelle.fr/wp-content/uploads/2021/07/logoPlan-de-travail-1.svg";
+  const logo = document.createElement('div');
+  logo.classList.add('logo');
   return logo;
 };
 
-const createSocialMenu = () => {
-  const arr = ["facebook", "instagram", "twitter"];
-  const div = document.createElement("div");
-
-  for (const i of arr) {
-    let a = document.createElement("a");
-    a.setAttribute("href", "#");
-    a.innerHTML = `<span>${i}</span>`;
-    div.appendChild(a);
-  }
-  return div;
-};
-
 const createNavBar = () => {
-  const navBar = document.createElement("nav");
+  const arr = ['home', 'menu', 'contact'];
+  const navBar = document.createElement('nav');
 
-  const homeButton = document.createElement("button");
-  homeButton.classList.add("button-nav");
-  homeButton.textContent = "Home";
+  const ul = document.createElement('ul');
+  arr.forEach((element) => {
+    const li = document.createElement('li');
+    li.textContent = element;
+    ul.appendChild(li);
+  });
 
-  const menuButton = document.createElement("button");
-  menuButton.classList.add("button-nav");
-  menuButton.textContent = "Carte";
-
-  const contactButton = document.createElement("button");
-  contactButton.classList.add("button-nav");
-  contactButton.textContent = "Contact";
-
-  navBar.appendChild(homeButton);
-  navBar.appendChild(menuButton);
-  navBar.appendChild(contactButton);
-
+  navBar.appendChild(ul);
   return navBar;
 };
 
 const createHeader = () => {
-  const header = document.createElement("header");
+  const header = document.createElement('header');
 
   header.appendChild(createLogo());
   header.appendChild(createNavBar());
-  header.appendChild(createSocialMenu());
+
   return header;
 };
 
