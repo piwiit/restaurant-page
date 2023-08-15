ccb7cfc393e4a1928ecd2de102609ffffc6bd166
piwiit <bouillaux@hotmail.fr>
2023-08-15 23:14:04 +0200

    clean code loading page

--- src/js/module/loading-page.js
+++ src/js/module/loading-page.js
@@ -1,17 +1,24 @@
 import loadHome from './home';
 import loadMenu from './menu';
 
-const createLogo = () => {
+const ELEMENTS = ['Home', 'Menu', 'Contact'];
+
+function createLogo() {
   const logo = document.createElement('img');
   logo.classList.add('logo');
   logo.setAttribute('rel', 'preload');
   logo.setAttribute('src', '../../picture/logo.png');
   logo.setAttribute('alt', 'logo du restaurant madera sur un fond blanc');
   return logo;
-};
+}
 
-const createNavBar = () => {
-  const arr = ['Home', 'Menu', 'Contact'];
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll('li');
+  buttons.forEach((btn) => btn.classList.remove('active'));
+  button.classList.add('active');
+}
+
+function createNavBar() {
   const navBar = document.createElement('nav');
   const ul = document.createElement('ul');
 
@@ -26,51 +33,41 @@ const createNavBar = () => {
         case 'Menu':
           loadMenu();
           setActiveButton(target);
-
           break;
         // Add more cases for other buttons if needed
       }
     }
   });
 
-  arr.forEach((element) => {
+  ELEMENTS.forEach((element, index) => {
     const li = document.createElement('li');
-    li.classList.add(`nav-button-${arr.indexOf(element)}`);
+    li.classList.add(`nav-button-${index}`);
     li.textContent = element;
     ul.appendChild(li);
   });
 
   navBar.appendChild(ul);
   return navBar;
-};
+}
 
-const setActiveButton = (button) => {
-  const buttons = document.querySelectorAll('li');
-  buttons.forEach((btn) => {
-    btn.classList.remove('active');
-  });
-  button.classList.add('active');
-};
-
-const createHeader = () => {
+function createHeader() {
   const header = document.createElement('header');
   header.appendChild(createLogo());
   header.appendChild(createNavBar());
   return header;
-};
+}
 
-const createMain = () => {
+function createMain() {
   const main = document.createElement('main');
   main.classList.add('main');
   return main;
-};
+}
 
-const initializeWebsite = () => {
+function initializeWebsite() {
   const app = document.getElementById('app');
-
   app.appendChild(createHeader());
   app.appendChild(createMain());
   loadHome();
-};
+}
 
 export default initializeWebsite;
