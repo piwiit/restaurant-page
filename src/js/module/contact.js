3400361c6482ceedc0854d723c4fab7ee4d13cb3
piwiit <bouillaux@hotmail.fr>
2023-08-21 23:03:19 +0200

    clean code contact-page

--- src/js/module/contact.js
+++ src/js/module/contact.js
@@ -1,44 +1,55 @@
 const createContact = () => {
+  // Créer le conteneur principal
   const contact = document.createElement('div');
   contact.classList.add('contact');
 
-  const phone = document.createElement('div');
-  phone.innerHTML = `<i class="fa-solid fa-phone"></i>`;
-  const phoneContact = document.createElement('p');
-  const phonenumber = document.createElement('p');
+  // Créer les éléments pour les informations de téléphone
+  const phone = createContactInfo('phone', 'fa-phone', 'Appelez nous:', '+33 07-98-16-89-24');
 
-  phoneContact.textContent = 'Appelez nous:';
-  phonenumber.textContent = '+33 07-98-16-89-24';
+  // Créer les éléments pour les informations d'email
+  const mail = createContactInfo('mail', 'fa-envelope', null, 'maderapizza@email.com');
 
-  const mail = document.createElement('div');
-  mail.innerHTML = `<i class="fa-solid fa-envelope"></i>`;
-  const email = document.createElement('p');
+  // Créer les éléments pour les informations d'adresse
+  const map = createContactInfo(
+    'map',
+    'fa-location-dot',
+    null,
+    "17 avenue de l'Amandier,  33000 Bordeaux"
+  );
 
-  email.textContent = 'maderapizza@email.com';
-
-  const map = document.createElement('div');
-  map.innerHTML = `<i class="fa-solid fa-location-dot"></i>`;
-  const street = document.createElement('p');
-  street.textContent = " 17 avenue de l'Amandier,  33000 Bordeaux";
-
+  // Ajouter les éléments au conteneur principal
   contact.appendChild(phone);
   contact.appendChild(mail);
   contact.appendChild(map);
 
-  phone.appendChild(phoneContact);
-  phone.appendChild(phonenumber);
-
-  mail.appendChild(email);
-
-  map.appendChild(street);
-
   return contact;
 };
 
-const loadcontact = () => {
+const createContactInfo = (type, iconClass, label, content) => {
+  const infoContainer = document.createElement('div');
+  infoContainer.classList.add(type);
+
+  const icon = document.createElement('i');
+  icon.classList.add('fa-solid', iconClass);
+
+  const infoContent = document.createElement('p');
+  if (label) {
+    const infoLabel = document.createElement('p');
+    infoLabel.textContent = label;
+    infoContainer.appendChild(infoLabel);
+  }
+  infoContent.textContent = content;
+
+  infoContainer.appendChild(icon);
+  infoContainer.appendChild(infoContent);
+
+  return infoContainer;
+};
+
+const loadContact = () => {
   const main = document.querySelector('.main');
   main.textContent = '';
   main.appendChild(createContact());
 };
 
-export default loadcontact;
+export default loadContact;
